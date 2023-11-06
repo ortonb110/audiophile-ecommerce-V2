e96803a78d26fb9bec65c7314092cdd070c171e2
Orton Bright <ortonb110@gmail.com>
2023-11-06 20:01:26 +0000

    Initial Setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D87D4A',
+        lightDar: '#101010',
+        lightGray: '#F1F1F1',
+        veryLightGray: '#FAFAFA',
+        primaryLight: '#fbaf85',
+        
+      }
+    },
+  },
+  plugins: [],
+}
