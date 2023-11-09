9bb277de49d1d1270e2ae885d1eb5bfaf1aecd1b
Orton Bright <ortonb110@gmail.com>
2023-11-09 20:56:57 +0000

    Created Hero Component

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+
+
+const Hero = () => {
+  return (
+    <header>
+        
+    </header>
+  )
+}
+
+export default Hero
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 
 const Home = () => {
   return (
-    <div className='px-[2.4rem] md:px-[3.9rem] lg:px-[11.6%] bg-black'>
-      <h1>home</h1>
+    <main className='px-[2.4rem] md:px-[3.9rem] lg:px-[11.6%] bg-black'>
+      
      
-    </div>
+    </main>
   )
 }
 
